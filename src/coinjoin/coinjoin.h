e12fb788d7df5b8490e8030f295361644de86f59
Setvin <117243445+realsetvin@users.noreply.github.com>
2025-09-02 08:26:34 -0400

    Update coinjoin.h

--- src/coinjoin/coinjoin.h
+++ src/coinjoin/coinjoin.h
@@ -451,8 +451,8 @@ public:
 
     /// If the collateral is valid given by a client
     static bool IsCollateralValid(const CTransaction& txCollateral);
-    static constexpr CAmount GetCollateralAmount() { return GetSmallestDenomination() / 1; }  /// the smallest coin join denomination will be 1
-    static constexpr CAmount GetMaxCollateralAmount() { return GetCollateralAmount() * 10001; }
+    static constexpr CAmount GetCollateralAmount() { return (GetSmallestDenomination() / 1) + 1; }      // 1*COIN + 1  /// the smallest coin join denomination will be 1
+    static constexpr CAmount GetMaxCollateralAmount() { return (GetSmallestDenomination() * 10000) + 10000; }  // 10000*COIN + 10000 please work
 
     static constexpr bool IsCollateralAmount(CAmount nInputAmount)
     {
