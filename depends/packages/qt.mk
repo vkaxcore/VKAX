79e421981808221e3432010031b28cdcfbcc6ad6
Setvin <117243445+realsetvin@users.noreply.github.com>
2025-09-04 18:16:11 -0400

    Update qt.mk

--- depends/packages/qt.mk
+++ depends/packages/qt.mk
@@ -2,25 +2,22 @@ PACKAGE=qt
 $(package)_version=5.15.10
 $(package)_download_path=https://download.qt.io/archive/qt/5.15/$($(package)_version)/single
 $(package)_suffix=opensource-src-$($(package)_version).tar.xz
-
-# Source tarballs
-$(package)_file_name=qtbase-$($(package)_suffix)
+$(package)_file_name=qt-everywhere-src-$($(package)_suffix)
 $(package)_sha256_hash=0c37696f3fa3cb4c0c1f9247fa38e7d946b2cfb53a36a67b61ff61877e3fca43
-$(package)_qttranslations_file_name=qttranslations-$($(package)_suffix)
-$(package)_qttranslations_sha256_hash=9822084f8e2d2939ba39f4af4c0c2320e45d5996762a9423f833055607604ed8
-$(package)_qttools_file_name=qttools-$($(package)_suffix)
-$(package)_qttools_sha256_hash=50e75417ec0c74bb8b1989d1d8e981ee83690dce7dfc0c2169f7c00f397e5117
-
-# Dependencies
 $(package)_dependencies=zlib
 ifeq ($(NO_OPENSSL),)
 $(package)_dependencies+= openssl
 endif
-
-# QT libraries to build
 $(package)_qt_libs=core gui widgets network
 
-# VKAX patches
+# Extra sources
+$(package)_qttranslations_file_name=qttranslations-$($(package)_suffix)
+$(package)_qttranslations_sha256_hash=9822084f8e2d2939ba39f4af4c0c2320e45d5996762a9423f833055607604ed8
+$(package)_qttools_file_name=qttools-$($(package)_suffix)
+$(package)_qttools_sha256_hash=50e75417ec0c74bb8b1989d1d8e981ee83690dce7dfc0c2169f7c00f397e5117
+$(package)_extra_sources  = $($(package)_qttranslations_file_name) $($(package)_qttools_file_name)
+
+# All VKAX patches
 $(package)_patches=\
   fix_qt_pkgconfig.patch \
   mac-qmake.conf \
@@ -40,83 +37,64 @@ $(package)_patches=\
   fix_qpainter_non_determinism.patch \
   fix_limits_header.patch
 
-# Extra sources
-$(package)_extra_sources  = $($(package)_qttranslations_file_name) $($(package)_qttools_file_name)
+define $(package)_set_vars
+$(package)_config_opts += -prefix $(host_prefix)
+$(package)_config_opts += -release
+$(package)_config_opts += -opensource
+ifeq ($(NO_OPENSSL),)
+$(package)_config_opts += -openssl-linked
+endif
+$(package)_config_opts += -silent
+$(package)_config_opts += -optimized-tools
+$(package)_config_opts += -no-pch
+$(package)_config_opts += -no-compile-examples
+$(package)_config_opts += -nomake examples
+$(package)_config_opts += -nomake tests
+$(package)_config_opts += -no-dbus
+$(package)_config_opts += -no-opengl
+$(package)_config_opts += -no-cups
+$(package)_config_opts += -no-feature-printdialog
+$(package)_config_opts += -no-feature-printer
+$(package)_config_opts += -no-feature-pdf
+$(package)_config_opts += -qt-libpng
+$(package)_config_opts += -qt-libjpeg
+$(package)_config_opts += -qt-harfbuzz
+$(package)_config_opts += -system-zlib
+$(package)_config_opts += -static
+$(package)_config_opts += -v
+endef
 
-# --- Fetch all sources ---
 define $(package)_fetch_cmds
-  $(call fetch_file,$(package),$($(package)_download_path),$($(package)_file_name),$($(package)_file_name),$($(package)_sha256_hash)) && \
-  $(call fetch_file,$(package),$($(package)_download_path),$($(package)_qttranslations_file_name),$($(package)_qttranslations_file_name),$($(package)_qttranslations_sha256_hash)) && \
-  $(call fetch_file,$(package),$($(package)_download_path),$($(package)_qttools_file_name),$($(package)_qttools_file_name),$($(package)_qttools_sha256_hash))
+$(call fetch_file,$(package),$($(package)_download_path),$($(package)_file_name),$($(package)_file_name),$($(package)_sha256_hash))
+$(call fetch_file,$(package),$($(package)_download_path),$($(package)_qttranslations_file_name),$($(package)_qttranslations_file_name),$($(package)_qttranslations_sha256_hash))
+$(call fetch_file,$(package),$($(package)_download_path),$($(package)_qttools_file_name),$($(package)_qttools_file_name),$($(package)_qttools_sha256_hash))
 endef
 
-# --- Extract all sources ---
 define $(package)_extract_cmds
-  mkdir -p $($(package)_extract_dir) && \
-  $(build_SHA256SUM) -c <(echo "$($(package)_sha256_hash)  $($(package)_source)"; \
-  echo "$($(package)_qttranslations_sha256_hash)  $($(package)_source_dir)/$($(package)_qttranslations_file_name)"; \
-  echo "$($(package)_qttools_sha256_hash)  $($(package)_source_dir)/$($(package)_qttools_file_name)")) && \
-  mkdir -p qtbase qttranslations qttools && \
-  tar --no-same-owner --strip-components=1 -xf $($(package)_source) -C qtbase && \
-  tar --no-same-owner --strip-components=1 -xf $($(package)_source_dir)/$($(package)_qttranslations_file_name) -C qttranslations && \
-  tar --no-same-owner --strip-components=1 -xf $($(package)_source_dir)/$($(package)_qttools_file_name) -C qttools
+mkdir -p $($(package)_extract_dir)
+tar --no-same-owner -xf $($(package)_source) -C $($(package)_extract_dir)/qtbase --strip-components=1
+tar --no-same-owner -xf $($(package)_source_dir)/$($(package)_qttranslations_file_name) -C $($(package)_extract_dir)/qttranslations --strip-components=1
+tar --no-same-owner -xf $($(package)_source_dir)/$($(package)_qttools_file_name) -C $($(package)_extract_dir)/qttools --strip-components=1
 endef
 
-# --- Preprocess: Apply VKAX patches ---
 define $(package)_preprocess_cmds
-  for patch in $($(package)_patches); do \
-    patch -p1 -i $($(package)_patch_dir)/$$patch; \
-  done
-  # Setup mac mkspecs
-  mkdir -p qtbase/mkspecs/macx-clang-linux && \
-  cp -f qtbase/mkspecs/macx-clang/qplatformdefs.h qtbase/mkspecs/macx-clang-linux/ && \
-  cp -f $($(package)_patch_dir)/mac-qmake.conf qtbase/mkspecs/macx-clang-linux/qmake.conf
+for patch in $($(package)_patches); do \
+  patch -p1 -i $($(package)_patch_dir)/$$patch; \
+done
 endef
 
-# --- Configure Qt ---
 define $(package)_config_cmds
-  cd qtbase && \
-  ./configure \
-    -prefix $(host_prefix) \
-    -release \
-    -opensource \
-    -optimized-tools \
-    -static \
-    -no-pch \
-    -no-compile-examples \
-    -nomake examples \
-    -nomake tests \
-    -no-dbus \
-    -no-opengl \
-    -no-cups \
-    -no-feature-printdialog \
-    -no-feature-printer \
-    -no-feature-pdf \
-    -qt-libpng \
-    -qt-libjpeg \
-    -qt-harfbuzz \
-    -system-zlib \
-    $(if $(NO_OPENSSL),,-openssl-linked)
+cd $($(package)_extract_dir)/qtbase && ./configure $($(package)_config_opts)
 endef
 
-# --- Build Qt ---
 define $(package)_build_cmds
-  $(MAKE) -C qtbase -j8 $(addprefix sub-,$($(package)_qt_libs)) && \
-  $(MAKE) -C qttools/src/linguist/lrelease && \
-  $(MAKE) -C qttools/src/linguist/lupdate && \
-  $(MAKE) -C qttranslations
+$(MAKE) -C $($(package)_extract_dir)/qtbase -j8
 endef
 
-# --- Stage Qt ---
 define $(package)_stage_cmds
-  $(MAKE) -C qtbase INSTALL_ROOT=$($(package)_staging_dir) $(addsuffix -install_subtargets,$(addprefix sub-,$($(package)_qt_libs))) && \
-  $(MAKE) -C qttools/src/linguist/lrelease INSTALL_ROOT=$($(package)_staging_dir) install_target && \
-  $(MAKE) -C qttools/src/linguist/lupdate INSTALL_ROOT=$($(package)_staging_dir) install_target && \
-  $(MAKE) -C qttranslations INSTALL_ROOT=$($(package)_staging_dir) install_subtargets
+$(MAKE) -C $($(package)_extract_dir)/qtbase INSTALL_ROOT=$($(package)_staging_dir) install
 endef
 
-# --- Postprocess ---
 define $(package)_postprocess_cmds
-  rm -rf native/mkspecs/ native/lib/ lib/cmake/ && \
-  rm -f lib/lib*.la lib/*.prl plugins/*/*.prl
+rm -rf $($(package)_staging_dir)/lib/cmake
 endef
