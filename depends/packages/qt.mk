a9ec878c1dca804f168fe546b988590ca1c7d541
Setvin <117243445+realsetvin@users.noreply.github.com>
2025-09-04 21:06:01 -0400

    Update qt.mk

--- depends/packages/qt.mk
+++ depends/packages/qt.mk
@@ -1,105 +1,128 @@
-package=qt
-$(package)_version=5.15.10
+PACKAGE=qt
 
-# Monolithic Qt 5.15.10
-$(package)_download_path=https://download.qt.io/archive/qt/5.15/$($(package)_version)/single
-$(package)_file_name=qt-everywhere-src-$($(package)_version).tar.xz
-$(package)_download_file=$($(package)_file_name)
+# ---- Version & sources (use single tarball) ----
+$(package)_version=5.15.10
+$(package)_download_path=https://download.qt.io/archive/qt/5.15/5.15.10/single
+$(package)_download_file=qt-everywhere-opensource-src-$($(package)_version).tar.xz
+$(package)_file_name=$(package)_download_file
+# keep your hash; update if checksum fails
 $(package)_sha256_hash=B545CB83C60934ADC9A6BBD27E2AF79E5013DE77D46F5B9F5BB2A3C762BF55CA
 
-# Optional submodules (harmless if also in monolith; helps some trees)
-$(package)_qttranslations_download_path=https://download.qt.io/archive/qt/5.15/$($(package)_version)/submodules
-$(package)_qttools_download_path=https://download.qt.io/archive/qt/5.15/$($(package)_version)/submodules
-$(package)_qttranslations_file_name=qttranslations-everywhere-opensource-src-$($(package)_version).tar.xz
-$(package)_qttools_file_name=qttools-everywhere-opensource-src-$($(package)_version).tar.xz
-$(package)_qttranslations_download_file=$($(package)_qttranslations_file_name)
-$(package)_qttools_download_file=$($(package)_qttools_file_name)
-$(package)_qttranslations_sha256_hash=38B942BC7E62794DD072945C8A92BB9DFFFED24070AEA300327A3BB42F855609
-$(package)_qttools_sha256_hash=66F46C9729C831DCE431778A9C561CCA32DACEAEDE1C7E58568D7A5898167DAE
-
+# ---- deps ----
 $(package)_dependencies=zlib
 ifeq ($(NO_OPENSSL),)
 $(package)_dependencies+=openssl
 endif
 
-# Build only libs VKAX links against (no testlib, no printsupport)
+# ---- what we build ----
 $(package)_qt_libs=corelib network widgets gui plugins
 
+# ---- patches you already carry (mostly no-ops for 5.15 but harmless) ----
+$(package)_patches = \
+    freetype_back_compat.patch \
+    fix_powerpc_libpng.patch \
+    drop_lrelease_dependency.patch \
+    dont_hardcode_pwd.patch \
+    fix_qt_pkgconfig.patch \
+    fix_configure_mac.patch \
+    fix_no_printer.patch \
+    fix_rcc_determinism.patch \
+    xkb-default.patch \
+    fix_android_qmake_conf.patch \
+    fix_android_jni_static.patch \
+    fix_riscv64_arch.patch \
+    no-xlib.patch \
+    fix_mingw_cross_compile.patch \
+    fix_qpainter_non_determinism.patch \
+    fix_limits_header.patch \
+    mac-qmake.conf
+
 define $(package)_set_vars
-  $(package)_config_opts = -release -silent -opensource -confirm-license -optimized-tools -static
-  $(package)_config_opts += -prefix $(host_prefix) -hostprefix $(build_prefix)
-  $(package)_config_opts += -no-compile-examples -nomake examples -nomake tests
-  $(package)_config_opts += -system-zlib -qt-libpng -qt-libjpeg -qt-harfbuzz
-  $(package)_config_opts += -no-icu -no-qml-debug -no-opengl
-  # nuke PrintSupport entirely
-  $(package)_config_opts += -no-feature-printer -no-feature-printdialog -no-feature-printpreviewdialog -no-feature-printpreviewwidget
+    $(package)_config_opts += -release -silent -opensource -optimized-tools -static
+    $(package)_config_opts += -confirm-license
+    $(package)_config_opts += -prefix $(host_prefix)
+    $(package)_config_opts += -hostprefix $(build_prefix)
+    $(package)_config_opts += -no-compile-examples -nomake examples -nomake tests
+    $(package)_config_opts += -qt-libpng -qt-libjpeg -qt-harfbuzz -system-zlib
 ifeq ($(NO_OPENSSL),)
-  $(package)_config_opts += -openssl-linked
+    $(package)_config_opts += -openssl-linked
 endif
+    # common feature trims to match legacy builds
+    $(package)_config_opts += -no-icu -no-dbus -no-sql -no-cups -no-gif -no-iconv
+    $(package)_config_opts += -no-opengl
 
-  # per-host opts (mac now, others later)
-  $(package)_config_opts_darwin = -no-dbus
-  $(package)_config_opts_aarch64_darwin += -device-option QMAKE_APPLE_DEVICE_ARCHS=arm64
+    # macOS
+    $(package)_config_opts_darwin += -no-opengl -no-dbus
 
-  $(package)_config_opts_linux  = -qt-xkbcommon-x11 -qt-xcb -no-xcb-xlib -no-feature-xlib
-  $(package)_config_opts_linux += -system-freetype -fontconfig -no-opengl
+    # Linux (kept for later cross builds)
+    $(package)_config_opts_linux  = -qt-xkbcommon-x11 -qt-xcb -no-xcb-xlib -no-feature-xlib
+    $(package)_config_opts_linux += -system-freetype -fontconfig -no-opengl
 
-  $(package)_config_opts_mingw32 = -no-opengl -no-dbus -xplatform win32-g++
+    # Linux ARM cross
+    $(package)_config_opts_arm_linux = -xplatform linux-g++ -device-option CROSS_COMPILE="$(host)-"
+    $(package)_config_opts_aarch64_linux = -xplatform linux-aarch64-gnu-g++
 
-  $(package)_build_env  = QT_RCC_TEST=1
-  $(package)_build_env += QT_RCC_SOURCE_DATE_OVERRIDE=1
+    # Windows / MinGW
+    $(package)_config_opts_mingw32 = -no-opengl -no-dbus -xplatform win32-g++
 endef
 
+# ---- fetch ----
 define $(package)_fetch_cmds
-  $(call fetch_file,$(package),$($(package)_download_path),$($(package)_download_file),$($(package)_file_name),$($(package)_sha256_hash)) && \
-  $(call fetch_file,$(package),$($(package)_qttranslations_download_path),$($(package)_qttranslations_download_file),$($(package)_qttranslations_file_name),$($(package)_qttranslations_sha256_hash)) && \
-  $(call fetch_file,$(package),$($(package)_qttools_download_path),$($(package)_qttools_download_file),$($(package)_qttools_file_name),$($(package)_qttools_sha256_hash))
+    $(call fetch_file,$(package),$($(package)_download_path),$($(package)_download_file),$($(package)_file_name),$($(package)_sha256_hash))
 endef
 
+# ---- extract (single tarball contains qtbase/qttools/qttranslations, etc.) ----
 define $(package)_extract_cmds
-  mkdir -p $($(package)_extract_dir) && \
-  tar --no-same-owner --strip-components=1 -xf $($(package)_source) -C $($(package)_extract_dir) && \
-  mkdir -p $($(package)_extract_dir)/qttranslations && \
-  tar --no-same-owner --strip-components=1 -xf $($(package)_source_dir)/$($(package)_qttranslations_file_name) -C $($(package)_extract_dir)/qttranslations && \
-  mkdir -p $($(package)_extract_dir)/qttools && \
-  tar --no-same-owner --strip-components=1 -xf $($(package)_source_dir)/$($(package)_qttools_file_name) -C $($(package)_extract_dir)/qttools
+    mkdir -p $($(package)_extract_dir) && \
+    tar --no-same-owner -xf $($(package)_source) -C $($(package)_extract_dir)
 endef
 
+# ---- preprocess ----
 define $(package)_preprocess_cmds
-  sed -i.old "s|updateqm.commands = \$$$$\$$$$LRELEASE|updateqm.commands = $($(package)_extract_dir)/qttools/bin/lrelease|" \
-    $($(package)_extract_dir)/qttranslations/translations/translations.pro
+    set -e; \
+    for p in $($(package)_patches); do \
+        patch -p1 -d $($(package)_extract_dir) < $($(package)_patch_dir)/$$p || true; \
+    done; \
+    mkdir -p $($(package)_extract_dir)/qtbase/mkspecs/macx-clang-linux && \
+    cp -f $($(package)_extract_dir)/qtbase/mkspecs/macx-clang/qplatformdefs.h \
+          $($(package)_extract_dir)/qtbase/mkspecs/macx-clang-linux/ && \
+    cp -f $($(package)_patch_dir)/mac-qmake.conf \
+          $($(package)_extract_dir)/qtbase/mkspecs/macx-clang-linux/qmake.conf
 endef
 
+# ---- configure ----
 define $(package)_config_cmds
-  export PKG_CONFIG_SYSROOT_DIR=/ && \
-  export PKG_CONFIG_LIBDIR=$(host_prefix)/lib/pkgconfig && \
-  export PKG_CONFIG_PATH=$(host_prefix)/share/pkgconfig && \
-  cd $($(package)_extract_dir)/qtbase && \
-  ./configure $($(package)_config_opts) $($(package)_config_opts_$(host_os)) $($(package)_config_opts_$(host_arch)_$(host_os)) && \
-  echo "host_build: QT_CONFIG ~= s/system-zlib/zlib" >> mkspecs/qconfig.pri && \
-  echo "CONFIG += force_bootstrap" >> mkspecs/qconfig.pri && \
-  cd .. && \
-  qtbase/bin/qmake -o qttranslations/Makefile qttranslations/qttranslations.pro && \
-  qtbase/bin/qmake -o qttranslations/translations/Makefile qttranslations/translations/translations.pro && \
-  qtbase/bin/qmake -o qttools/src/linguist/lrelease/Makefile qttools/src/linguist/lrelease/lrelease.pro && \
-  qtbase/bin/qmake -o qttools/src/linguist/lupdate/Makefile qttools/src/linguist/lupdate/lupdate.pro
+    export PKG_CONFIG_SYSROOT_DIR=/; \
+    export PKG_CONFIG_LIBDIR=$(host_prefix)/lib/pkgconfig; \
+    export PKG_CONFIG_PATH=$(host_prefix)/share/pkgconfig; \
+    cd $($(package)_extract_dir)/qtbase && \
+    ./configure $($(package)_config_opts) && \
+    echo "host_build: QT_CONFIG ~= s/system-zlib/zlib" >> mkspecs/qconfig.pri && \
+    echo "CONFIG += force_bootstrap" >> mkspecs/qconfig.pri && \
+    cd $($(package)_extract_dir) && \
+    qtbase/bin/qmake -o qttranslations/Makefile qttranslations/qttranslations.pro && \
+    qtbase/bin/qmake -o qttools/src/linguist/lrelease/Makefile qttools/src/linguist/lrelease/lrelease.pro && \
+    qtbase/bin/qmake -o qttools/src/linguist/lupdate/Makefile qttools/src/linguist/lupdate/lupdate.pro
 endef
 
+# ---- build ----
 define $(package)_build_cmds
-  $(MAKE) -C $($(package)_extract_dir)/qtbase/src $(addprefix sub-,$($(package)_qt_libs)) && \
-  $(MAKE) -C $($(package)_extract_dir)/qttools/src/linguist/lrelease && \
-  $(MAKE) -C $($(package)_extract_dir)/qttools/src/linguist/lupdate && \
-  $(MAKE) -C $($(package)_extract_dir)/qttranslations
+    $(MAKE) -C $($(package)_extract_dir)/qtbase/src $(addprefix sub-,$($(package)_qt_libs)) && \
+    $(MAKE) -C $($(package)_extract_dir)/qttools/src/linguist/lrelease && \
+    $(MAKE) -C $($(package)_extract_dir)/qttools/src/linguist/lupdate && \
+    $(MAKE) -C $($(package)_extract_dir)/qttranslations
 endef
 
+# ---- stage ----
 define $(package)_stage_cmds
-  $(MAKE) -C $($(package)_extract_dir)/qtbase/src INSTALL_ROOT=$($(package)_staging_dir) \
-      $(addsuffix -install_subtargets,$(addprefix sub-,$($(package)_qt_libs))) && \
-  $(MAKE) -C $($(package)_extract_dir)/qttools/src/linguist/lrelease INSTALL_ROOT=$($(package)_staging_dir) install_target && \
-  $(MAKE) -C $($(package)_extract_dir)/qttools/src/linguist/lupdate  INSTALL_ROOT=$($(package)_staging_dir) install_target && \
-  $(MAKE) -C $($(package)_extract_dir)/qttranslations INSTALL_ROOT=$($(package)_staging_dir) install_subtargets
+    $(MAKE) -C $($(package)_extract_dir)/qtbase/src INSTALL_ROOT=$($(package)_staging_dir) \
+        $(addsuffix -install_subtargets,$(addprefix sub-,$($(package)_qt_libs))) && \
+    $(MAKE) -C $($(package)_extract_dir)/qttools/src/linguist/lrelease INSTALL_ROOT=$($(package)_staging_dir) install_target && \
+    $(MAKE) -C $($(package)_extract_dir)/qttools/src/linguist/lupdate  INSTALL_ROOT=$($(package)_staging_dir) install_target && \
+    $(MAKE) -C $($(package)_extract_dir)/qttranslations               INSTALL_ROOT=$($(package)_staging_dir) install_subtargets
 endef
 
+# ---- tidy ----
 define $(package)_postprocess_cmds
-  rm -rf lib/cmake/ && rm -f lib/lib*.la lib/*.prl plugins/*/*.prl
+    rm -rf $($(package)_staging_dir)/lib/cmake
 endef
