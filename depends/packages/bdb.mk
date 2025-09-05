b64f0a39a736521558fc84abedc59402b35a21fb
Setvin <117243445+realsetvin@users.noreply.github.com>
2025-09-05 19:19:52 -0400

    Update bdb.mk

--- depends/packages/bdb.mk
+++ depends/packages/bdb.mk
@@ -9,48 +9,46 @@ $(package)_build_subdir=build_unix
 $(package)_patches=clang_cxx_11.patch
 
 define $(package)_set_vars
-  # static lib only + C++ API; PIC on linux/android to satisfy NDK linkers
-  $(package)_config_opts=--disable-shared --enable-cxx --disable-replication
-  $(package)_config_opts_linux=--with-pic
-  $(package)_config_opts_android=--with-pic
-  $(package)_config_opts_mingw32=--enable-mingw
-  $(package)_cxxflags=-std=c++17
+	# static lib only + C++ API; PIC on linux/android to satisfy NDK linkers
+	$(package)_config_opts=--disable-shared --enable-cxx --disable-replication
+	$(package)_config_opts_linux=--with-pic
+	$(package)_config_opts_android=--with-pic
+	$(package)_config_opts_mingw32=--enable-mingw
+	$(package)_cxxflags=-std=c++17
 endef
 
 define $(package)_preprocess_cmds
-  patch -p1 < $($(package)_patch_dir)/clang_cxx_11.patch && \
-  cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub dist
+	patch -p1 < $($(package)_patch_dir)/clang_cxx_11.patch && \
+	cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub dist
 endef
 
 define $(package)_config_cmds
-  # NOTE: prefix must be the depends host prefix so DESTDIR staging lands in ${staging_dir}/${host_prefix}
-  ../dist/configure --prefix=$(host_prefix) $($(package)_config_opts)
+	../dist/configure --prefix=$(host_prefix) $($(package)_config_opts)
 endef
 
 define $(package)_build_cmds
-  $(MAKE) libdb-4.8.a libdb_cxx-4.8.a
+	$(MAKE) libdb-4.8.a libdb_cxx-4.8.a
 endef
 
 define $(package)_stage_cmds
-  # install headers/libs into staging; we’ll normalize layout in postprocess
-  $(MAKE) DESTDIR=$($(package)_staging_dir) install_lib install_include
+	$(MAKE) DESTDIR=$($(package)_staging_dir) install_lib install_include
 endef
 
 define $(package)_postprocess_cmds
-  # move headers into versioned include dir expected by legacy autoconf checks
-  mkdir -p $($(package)_staging_prefix_dir)/include/db4.8 && \
-  if [ -d "$($(package)_staging_dir)/$(host_prefix)/include" ]; then \
-    cd $($(package)_staging_dir)/$(host_prefix)/include && \
-    for h in db.h db_cxx.h db_185.h db_cxx_mpool.h; do \
-      [ -f "$$h" ] && cp -f "$$h" db4.8/; \
-    done; \
-  fi && \
-  # ensure versioned libs exist; provide unversioned fallbacks to satisfy alt checks
-  cd $($(package)_staging_dir)/$(host_prefix)/lib && \
-  [ -f libdb_cxx-4.8.a ] || { [ -f libdb_cxx.a ] && cp -f libdb_cxx.a libdb_cxx-4.8.a; } && \
-  [ -f libdb-4.8.a     ] || { [ -f libdb.a     ] && cp -f libdb.a     libdb-4.8.a; } && \
-  [ -f libdb_cxx.a     ] || cp -f libdb_cxx-4.8.a libdb_cxx.a && \
-  [ -f libdb.a         ] || cp -f libdb-4.8.a     libdb.a
+	# move headers into versioned include dir expected by legacy autoconf checks
+	mkdir -p $($(package)_staging_prefix_dir)/include/db4.8 && \
+	if [ -d "$($(package)_staging_dir)/$(host_prefix)/include" ]; then \
+		cd $($(package)_staging_dir)/$(host_prefix)/include && \
+		for h in db.h db_cxx.h db_185.h db_cxx_mpool.h; do \
+			[ -f "$$h" ] && cp -f "$$h" db4.8/; \
+		done; \
+	fi && \
+	# ensure versioned libs exist; provide unversioned fallbacks to satisfy alt checks
+	cd $($(package)_staging_dir)/$(host_prefix)/lib && \
+	[ -f libdb_cxx-4.8.a ] || { [ -f libdb_cxx.a ] && cp -f libdb_cxx.a libdb_cxx-4.8.a; } && \
+	[ -f libdb-4.8.a     ] || { [ -f libdb.a     ] && cp -f libdb.a     libdb-4.8.a; } && \
+	[ -f libdb_cxx.a     ] || cp -f libdb_cxx-4.8.a libdb_cxx.a && \
+	[ -f libdb.a         ] || cp -f libdb-4.8.a     libdb.a
 endef
 
 # Signed: Setvin
