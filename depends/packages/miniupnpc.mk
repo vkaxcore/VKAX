3b748d3b82d6ecf515cf7d596011a764b7bff290
Setvin <117243445+realsetvin@users.noreply.github.com>
2025-09-05 19:14:26 -0400

    Update miniupnpc.mk

--- depends/packages/miniupnpc.mk
+++ depends/packages/miniupnpc.mk
@@ -1,3 +1,5 @@
+# depends/packages/miniupnpc.mk
+# VKAX — miniupnpc static build for Android cross; PIC + deterministic toolchain/env; headers into include/miniupnpc, lib into lib. — Setvin
 package=miniupnpc
 $(package)_version=2.0.20180203
 $(package)_download_path=https://miniupnp.tuxfamily.org/files/
@@ -6,24 +8,29 @@ $(package)_sha256_hash=90dda8c7563ca6cd4a83e23b3c66dbbea89603a1675bfdb852897c2c9
 $(package)_patches=dont_use_wingen.patch
 
 define $(package)_set_vars
-$(package)_build_opts=CC="$($(package)_cc)"
-$(package)_build_opts_darwin=LIBTOOL="$($(package)_libtool)"
-$(package)_build_opts_mingw32=-f Makefile.mingw
-$(package)_build_env+=CFLAGS="$($(package)_cflags) $($(package)_cppflags)" AR="$($(package)_ar)"
+  # ensure PIC for Android; wire AR/RANLIB explicitly for reproducibility
+  $(package)_cflags+=-fPIC
+  $(package)_build_opts=CC="$($(package)_cc)" AR="$($(package)_ar)" RANLIB="$(host_RANLIB)"
+  $(package)_build_opts_darwin=LIBTOOL="$($(package)_libtool)"
+  $(package)_build_opts_mingw32=-f Makefile.mingw
+  $(package)_build_env+=CFLAGS="$($(package)_cflags) $($(package)_cppflags)" LDFLAGS="$($(package)_ldflags)"
 endef
 
 define $(package)_preprocess_cmds
-  mkdir dll && \
-  sed -e 's|MINIUPNPC_VERSION_STRING \"version\"|MINIUPNPC_VERSION_STRING \"$($(package)_version)\"|' -e 's|OS/version|$(host)|' miniupnpcstrings.h.in > miniupnpcstrings.h && \
+  mkdir -p dll && \
+  sed -e 's|MINIUPNPC_VERSION_STRING \"version\"|MINIUPNPC_VERSION_STRING \"$($(package)_version)\"|' \
+      -e 's|OS/version|$(host)|' miniupnpcstrings.h.in > miniupnpcstrings.h && \
   patch -p1 < $($(package)_patch_dir)/dont_use_wingen.patch
 endef
 
 define $(package)_build_cmds
-	$(MAKE) libminiupnpc.a $($(package)_build_opts)
+  $(MAKE) libminiupnpc.a $($(package)_build_opts)
 endef
 
 define $(package)_stage_cmds
-	mkdir -p $($(package)_staging_prefix_dir)/include/miniupnpc $($(package)_staging_prefix_dir)/lib &&\
-	install *.h $($(package)_staging_prefix_dir)/include/miniupnpc &&\
-	install libminiupnpc.a $($(package)_staging_prefix_dir)/lib
+  mkdir -p $($(package)_staging_prefix_dir)/include/miniupnpc $($(package)_staging_prefix_dir)/lib && \
+  install -m644 *.h $($(package)_staging_prefix_dir)/include/miniupnpc/ && \
+  install -m644 libminiupnpc.a $($(package)_staging_prefix_dir)/lib/
 endef
+
+# Signed: Setvin
